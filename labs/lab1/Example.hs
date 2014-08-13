50e5c1dd46852587e364deabe66946bcbddfb582
Andrés Sicard-Ramírez <asr@eafit.edu.co>
2014-08-13 08:17:10 -0500

    Using 'accepting' instead of 'final'

--- labs/lab1/Example.hs
+++ labs/lab1/Example.hs
@@ -13,14 +13,14 @@ symbols1 = Set.fromList "01"
 initialState1 :: String
 initialState1 = "q0"
 
-finalStates1 :: Set String
-finalStates1 = Set.fromList ["q2"]
+acceptingStates1 :: Set String
+acceptingStates1 = Set.fromList ["q2"]
 
 errorState1 :: Set String
 errorState1 = Set.singleton "qq"
 
 case1 :: NFA String Char
-case1 = MkNFA states1 symbols1 delta initialState1 finalStates1
+case1 = MkNFA states1 symbols1 delta initialState1 acceptingStates1
   where
     delta :: String -> Char -> Set String
     delta "q0" '0' = Set.fromList ["q0", "q1"]
--- labs/lab1/NFA.hs
+++ labs/lab1/NFA.hs
@@ -4,9 +4,9 @@ import Data.Set ( Set )
 
 -- | A non-deterministic finite automaton.
 data NFA state symbol =
-  MkNFA { states  :: Set state                    --  States
-        , symbols :: Set symbol                   --  Input symbols
-        , delta   :: state -> symbol -> Set state --  Transition function
-        , start   :: state                        --  Start state
-        , finals  :: Set state                    --  Final states
+  MkNFA { states    :: Set state                    --  States
+        , symbols   :: Set symbol                   --  Input symbols
+        , delta     :: state -> symbol -> Set state --  Transition function
+        , start     :: state                        --  Start state
+        , accepting :: Set state                    --  Accepting states
         }
